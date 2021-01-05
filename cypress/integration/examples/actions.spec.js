4955f57d960ff78f8e4f56722a8b707036d7915d
Mandi <haase1020@gmail.com>
2021-01-05 18:42:25 -0500

    add cypress

---
+++ cypress/integration/examples/actions.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+context("Actions", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it("should vitist website", () => {
+    // https://on.cypress.io/type
+    cy.get(".btn").should("exist");
+  });
+});
